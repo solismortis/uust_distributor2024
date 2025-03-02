7b3100aad86f39dc466bf73176d14986d49abc8d
SoliS Mortis <callmesolis@gmail.com>
2025-03-02 18:01:19 +0500

    Обновлено README.md

--- main.py
+++ main.py
@@ -53,10 +53,6 @@ def add_2_comp_groops(html):
                                   'places': places,
                                   'basis': basis})
 
-    def convert():
-        
-
-
     tables_all = pd.read_html(html, converters={'Уникальный код': str})
     for i, df in enumerate(tables_all):
         local_comp_groups[i]['df'] = df[['Уникальный код',
