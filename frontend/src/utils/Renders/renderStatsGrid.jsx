cda8ec033a2bcf2c8efce99a36c19261d44b087f
Prosto Shoma <shomailganiev@yandex.ru>
2025-03-26 05:53:42 +0500

    подогнал под наш json

--- frontend/src/utils/Renders/renderStatsGrid.jsx
+++ frontend/src/utils/Renders/renderStatsGrid.jsx
@@ -5,10 +5,10 @@ function renderStatsGrid(json) {
     course => {
       return (<>
               <div className='item div-course-name'>
-                {course.title}
+                {course.text}
               </div>
               <div className="item div-abit-rank" style={{textAlign:"center"}}>
-                Ваша позиция: 1
+                {course.groups?.map(group => { return (<div style={{ margin: "10px 0" }}>{group}</div>) })}
               </div>
               <div className="item div-unfold-info" style={{textAlign:"center", cursor:"pointer"}}>
                 Подробнее &#8595;
