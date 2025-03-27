65ec437053c10e9589ddcb66e4356047f1ef91fd
SoliS Mortis <callmesolis@gmail.com>
2025-03-27 20:23:27 +0500

    Чистка файлов и переименование всех comp_group в group

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.max_colwidth', None)
 
 
-def add_to_comp_groups(html):
-    """ Добавление групп из html в comp_groups"""
-    global comp_groups
-    local_comp_groups = []
+def add_to_groups(html):
+    """ Добавление групп из html в groups"""
+    global groups
+    local_groups = []
     fileObj = codecs.open(html, "r", "utf_8_sig")
     text = fileObj.read()
     text = text[re.search(r'<div class="list-specialty">.*</div>', text).end():]  # 1-й результат не наш
@@ -29,7 +29,7 @@ def add_to_comp_groups(html):
         s = re.search(r'<li>Конкурсная группа: <span>.*</span></li>', el).group()
         start = re.search(r'<li>Конкурсная группа: <span>', s).end()
         end = re.search(r'</span>', s).start()
-        comp_group = s[start:end]
+        group = s[start:end]
 
         s = re.search(r'<li>Свободно мест: <span>\d+</span></li>', el).group()
         start = re.search(r'<li>Свободно мест: <span>', s).end()
@@ -42,29 +42,29 @@ def add_to_comp_groups(html):
         end = re.search(r'</span>', s).start()
         basis = s[start:end]
 
-        local_comp_groups.append({'comp_group': comp_group,
+        local_groups.append({'group': group,
                                   'places': places,
                                   'basis': basis})
 
     tables_all = pd.read_html(html, converters={'Уникальный код': str,
                                                 'Приоритет': lambda el: int(re.search(r'\d*', el).group())})
     for i, df in enumerate(tables_all):
-        local_comp_groups[i]['df'] = df[['Уникальный код',
+        local_groups[i]['df'] = df[['Уникальный код',
                                          'Сумма баллов',
                                          'Приоритет']].rename(columns={'Уникальный код': 'id',
                                                                        'Сумма баллов': 'score',
                                                                        'Приоритет': 'prio'})
 
     # Удаляем коммерцию
-    local_comp_groups1 = []
-    for el in local_comp_groups:
+    local_groups1 = []
+    for el in local_groups:
         if el['basis'] != 'Полное возмещение затрат':
-            local_comp_groups1.append(el)
-    local_comp_groups = local_comp_groups1
+            local_groups1.append(el)
+    local_groups = local_groups1
 
-    # Пихаем все в global comp_groups
-    for el in local_comp_groups:
-        comp_groups[el['comp_group']] = {'places': el['places'],
+    # Пихаем все в global groups
+    for el in local_groups:
+        groups[el['group']] = {'places': el['places'],
                                          'basis': el['basis'],
                                          'df': el['df']}
 
@@ -72,19 +72,19 @@ def add_to_comp_groups(html):
 def create_abits():
     """ Создаем dict для приоритетов абитуриентов """
     abits = {}
-    for el_key, el_val in original_comp_groups.items():
+    for el_key, el_val in original_groups.items():
         df = el_val['df']
         to_drop = []
         for index, row in df.iterrows():
             abit = row['id']
             if abit not in abits:
                 abits[abit] = {}
-                abits[abit][row['prio']] = {'comp_group': el_key, 'burnt': False}
+                abits[abit][row['prio']] = {'group': el_key, 'burnt': False}
             else:
                 if row['prio'] in abits[abit]:  # Фикс бага с дубликатами прио
                     to_drop.append(index)
                 else:
-                    abits[abit][row['prio']] = {'comp_group': el_key, 'burnt': False}
+                    abits[abit][row['prio']] = {'group': el_key, 'burnt': False}
         if to_drop:
             df.drop(to_drop)
     return abits
@@ -93,13 +93,13 @@ def create_abits():
 def process_id(id):
     """Обработка ID. Возвращает json"""
     # Поиск конкурсных групп и баллов выбранного абита
-    id_comp_groups = []
+    id_groups = []
     id_score = None
-    for el_key, el_val in original_comp_groups.items():
+    for el_key, el_val in original_groups.items():
         df = el_val['df']
         df1 = df[df['id'] == id].dropna()
         if not df1.empty:
-            id_comp_groups.append(el_key)
+            id_groups.append(el_key)
             id_score = int(df1['score'].iloc[0])
 
     # Обработка позиций id. Ввиду того, как работает сортировка, на принт уйдет только позиция с зачислением,
@@ -107,13 +107,13 @@ def process_id(id):
     arr = []
     for distributed in (False, True):
         dict0 = {}
-        groups = original_comp_groups if not distributed else sorted_groups
+        groups = original_groups if not distributed else sorted_groups
         if not distributed:
             dict0['text'] = f'Позиции id {id} до распределения:'
         else:
             dict0['text'] = f'Позиции id {id} после распределения:'
         dict0['groups'] = []
-        for group in id_comp_groups:
+        for group in id_groups:
             df = groups[group]['df']
             df1 = df[df['id'] == id].dropna()  # Удаляет пустые строки?
             if not df1.empty:  # Срабатывает до распределения
@@ -129,7 +129,7 @@ def process_id(id):
     return json.dumps(arr, indent=4, ensure_ascii=False)
 
 
-def sorting_algo(comp_groups, abits):
+def sorting_algo(groups, abits):
     """ Сортировка. Делаем while loop по всем таблицам; в каждой выбираем всех вмещающихся абитов,
     смотрим их прио, если все прио перед этим прио сгорели (здесь под "сгорел" понимается вылет из конкурсной группы ввиду
     отсутствия мест), то зачисляем; убираем абита из остальных таблиц; если таблица обработана, выкидываем ее в
@@ -137,9 +137,9 @@ def sorting_algo(comp_groups, abits):
     print('Сортировка\n')
     sorted_groups = {}
     c = 0
-    while comp_groups:
+    while groups:
         to_pop = []
-        for el_key, el_val in comp_groups.items():
+        for el_key, el_val in groups.items():
             places = el_val['places']
             df = el_val['df']
             sliced_df = df.iloc[:places]  # Выбираем только вмещающихся
@@ -162,9 +162,9 @@ def sorting_algo(comp_groups, abits):
                         # Убираем этого абитуриента из остальных таблиц
                         for prio_key, prio_val in abits[abit].items():
                             prio_val['burnt'] = True  # Сжечь все
-                            if prio_val['comp_group'] != el_key:  # Только другие таблицы
+                            if prio_val['group'] != el_key:  # Только другие таблицы
                                 try:
-                                    df1 = comp_groups[prio_val['comp_group']]['df']
+                                    df1 = groups[prio_val['group']]['df']
                                     df2 = df1[df1['id'] == abit].dropna()
                                     if not df2.empty:
                                         i = df2.index
@@ -192,37 +192,37 @@ def sorting_algo(comp_groups, abits):
                 # Может быть такое: 09.03.04 Программная инженерия, Очная, Бюджет, Целевая, СЭГЗ АО
                 # for x in ['особая', 'отдельная', 'целевая']:
                 #     if x in el['Конкурсная группа']:
-                #         for el2 in comp_groups:
+                #         for el2 in groups:
                 #             s = el2['Конкурсная группа'][:-len(x)-2]
                 #             if s == s + ', Общая':
                 #                 el2['Свободно мест'] += el['Свободно мест']
         c += 1
         for pop_el in to_pop:
-            sorted_groups[pop_el] = comp_groups.pop(pop_el, None)
+            sorted_groups[pop_el] = groups.pop(pop_el, None)
         print(f'Пробег №: {c}')
     print()
     return sorted_groups
 
 
-comp_groups = {}  # Основной dict для конкурсных групп. Очищается по мере сортировки
-original_comp_groups = {}
+groups = {}  # Основной dict для конкурсных групп. Очищается по мере сортировки
+original_groups = {}
 
 def process_html():
     """ Парсинг html """
-    global comp_groups
-    global original_comp_groups
+    global groups
+    global original_groups
     # Пробегаемся по всем сохраненным html
     files = os.listdir('./html files')
     for filename in files:
         find = re.search(r'\.html', filename)
         if find:
-            add_to_comp_groups('./html files/' + filename)
-    original_comp_groups = copy.deepcopy(comp_groups)
+            add_to_groups('./html files/' + filename)
+    original_groups = copy.deepcopy(groups)
 
 process_html()
 
 abits = create_abits()
-sorted_groups = sorting_algo(comp_groups, abits)
+sorted_groups = sorting_algo(groups, abits)
 
 json_object = process_id(id='151-464-963 67')
 print(json_object)
@@ -231,7 +231,7 @@ json_object = process_id(id='139-925-279 07')
 print(json_object)
 
 # Аналитика
-# def return_processed_comp_groups(groups_of_interest):
+# def return_processed_groups(groups_of_interest):
 #     arr = []
 #     for group in groups_of_interest:
 #         group1 = copy.deepcopy(sorted_groups[group])
@@ -242,5 +242,5 @@ print(json_object)
 # groups_of_interest = ['02.03.03 Технологиии искусственного интеллекта, Очная, Бюджет, Общая',
 #                       '03.03.01 Моделирование физических процессов и технологий, Очная, Бюджет, Отдельная',
 #                       '06.03.01 Общая биология (Сибайский институт), Очная, Бюджет, Общая']
-# print(return_processed_comp_groups(groups_of_interest))
+# print(return_processed_groups(groups_of_interest))
 pass
