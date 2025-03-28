a89b926341f1fa6225581e59264ca1ede7291a93
Ximunda <88gaster88@gmail.com>
2025-03-28 15:02:41 +0500

    Сделал так, чтобы обновлялись HTML`ки каждый день в 02:00. При этом сервер будет работать пока происходит обновление.

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+from calendar import day_name
+from datetime import datetime
+
+from flask_cors.core import serialize_option
 from numpy.f2py.rules import options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,27 +9,60 @@ from flask import *
 from flask_cors import CORS
 import json
 import main
+import threading
+import time
 
-options = webdriver.FirefoxOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-gpu")
-driver = webdriver.Firefox(options = options)
+def update_html():
+    while True:
+        print("Ожидание...")
+        now = datetime.now()
+        if now.strftime("%H:%M") == "02:00":
+            options = webdriver.FirefoxOptions()
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            driver = webdriver.Firefox(options = options)
 
-url = "https://list.uust.ru/spisok.php?levelTarget=bak&specialty=01.03.02+%D0%9F%D1%80%D0%B8%D0%BA%D0%BB%D0%B0%D0%B4%D0%BD%D0%B0%D1%8F+%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0+%D0%B8+%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0&footings=%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82%D0%BD%D0%B0%D1%8F+%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%B0&search_type=snils&snls=17399553731&group=01.03.02+%D0%98%D1%81%D0%BA%D1%83%D1%81%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9+%D0%B8%D0%BD%D1%82%D0%B5%D0%BB%D0%BB%D0%B5%D0%BA%D1%82+%D0%B8+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7+%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85+%28%D0%A1%D1%82%D0%B5%D1%80%D0%BB%D0%B8%D1%82%D0%B0%D0%BC%D0%B0%D0%BA%D1%81%D0%BA%D0%B8%D0%B9+%D1%84%D0%B8%D0%BB%D0%B8%D0%B0%D0%BB%29%2C+%D0%9E%D1%87%D0%BD%D0%B0%D1%8F%2C+%D0%91%D1%8E%D0%B4%D0%B6%D0%B5%D1%82%2C+%D0%9E%D0%B1%D1%89%D0%B0%D1%8F&profile="
-driver.get(url)
+            url = "https://list.uust.ru/spisok.php?levelTarget=vo_rang&specialty=&original=originalAll&search_type=snils&snls="
+            driver.get(url)
+            print("Началось обновление html")
+            for i in range(3,4): #здесь нужно указать от трех до числа специальностей (110)
+                search_box = driver.find_element(By.NAME, "specialty")
+                search_box.click()
 
-html = driver.find_element(By.TAG_NAME, "html").get_attribute("outerHTML")
-print(html)
+                search_box = driver.find_element(By.CSS_SELECTOR, f"div.list-filter-element4:nth-child(3) > select:nth-child(1) > option:nth-child({i})")
+                spec_name = search_box.get_attribute("value")
+                search_box.click()
+                html_update = driver.find_element(By.TAG_NAME, "html").get_attribute("outerHTML")
 
+                html_old = open(f"html files/NeW-{spec_name}.html", "w+", encoding="utf-8") #пока что создаю новые html на случай если формат все же не подойдет
+                html_old.write(html_update)
+                html_old.close()
+
+                search_box = driver.find_element(By.NAME, "specialty")
+                search_box.click()
+
+                search_box = driver.find_element(By.CSS_SELECTOR, "div.list-filter-element4:nth-child(3) > select:nth-child(1) > option:nth-child(2)")
+                search_box.click()
+            driver.quit()
+            print("Обновление завершилось")
+        time.sleep(60)
 
 serv=Flask(__name__)
 CORS(serv)
+@serv.route('/')
+def home():
+    return "Server is online now"
 
 @serv.route('/search')
 def serc():
     user = request.args.get('id')
     return main.process_id(user)
 
-if __name__ != "__main__":
-    serv.run()
+def start_background_task():
+    thread = threading.Thread(target=update_html, daemon=True)
+    thread.start()
+
+if __name__ == "__main__":
+    start_background_task()
+    serv.run(debug=True, use_reloader=False)
